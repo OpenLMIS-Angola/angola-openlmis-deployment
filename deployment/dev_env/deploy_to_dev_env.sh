1095ee3543876e77563d4d26dc6fe05c50bf30ee
Ben Leibert <ben.leibert@villagereach.org>
2018-12-11 12:37:27 -0800

    Add dev-environment files

---
+++ deployment/dev_env/deploy_to_dev_env.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+export DOCKER_TLS_VERIFY="1"
+export DOCKER_HOST="development.ao.openlmis.org:2376"
+export DOCKER_CERT_PATH="${PWD}/../../credentials"
+
+../shared/init_env.sh
+
+/usr/local/bin/docker-compose pull
+
+../shared/restart_or_restore.sh $1

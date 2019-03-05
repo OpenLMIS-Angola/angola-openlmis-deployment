6ee52fd5c1000e36322e10925864d98717b78a44
Mateusz Kwiatkowski <mkwiatkowski@soldevelo.com>
2019-03-05 19:36:13 +0100

    AO-75: added initiating env file

--- deployment/production_env/deploy_to_production_env.sh
+++ deployment/production_env/deploy_to_production_env.sh
@@ -4,6 +4,8 @@ export DOCKER_TLS_VERIFY="1"
 export DOCKER_HOST="34.224.236.66:2376"
 export DOCKER_CERT_PATH="${PWD}/../../credentials"
 
+../shared/init_env.sh
+
 /usr/local/bin/docker-compose pull
 
 ../shared/restart_or_restore.sh $1

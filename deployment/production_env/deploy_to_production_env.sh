5c7ad16492e3c7a5e249096cda7dd75e5cafaae1
Mateusz Kwiatkowski <mkwiatkowski@soldevelo.com>
2019-03-12 17:48:22 +0100

    AO-75: fixed path to production docker certificates

--- deployment/production_env/deploy_to_production_env.sh
+++ deployment/production_env/deploy_to_production_env.sh
@@ -2,7 +2,7 @@
 
 export DOCKER_TLS_VERIFY="1"
 export DOCKER_HOST="production.ao.openlmis.org:2376"
-export DOCKER_CERT_PATH="${PWD}/../../credentials"
+export DOCKER_CERT_PATH="${PWD}/../../credentials/production_env"
 
 /usr/local/bin/docker-compose pull
 

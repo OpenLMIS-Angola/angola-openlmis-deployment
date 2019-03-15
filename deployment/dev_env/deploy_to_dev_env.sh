818c5be4cc83fa5fc456f00be226a5841a18bb58
Mateusz Kwiatkowski <mkwiatkowski@soldevelo.com>
2019-03-15 12:19:59 +0100

    removed init env step from deploy scripts as it is done in Jenkins job

--- deployment/dev_env/deploy_to_dev_env.sh
+++ deployment/dev_env/deploy_to_dev_env.sh
@@ -4,8 +4,6 @@ export DOCKER_TLS_VERIFY="1"
 export DOCKER_HOST="development.ao.openlmis.org:2376"
 export DOCKER_CERT_PATH="${PWD}/../../credentials/dev_env"
 
-../shared/init_env.sh
-
 /usr/local/bin/docker-compose pull
 
 ../shared/restart_or_restore.sh $1

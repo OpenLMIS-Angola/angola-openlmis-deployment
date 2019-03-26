5bc21dccd65a64ce0d370410bcfc91b5c4e47058
Mateusz Kwiatkowski <mkwiatkowski@soldevelo.com>
2019-03-26 17:44:09 +0100

    AO-96: added logging for sql updates

--- deployment/shared/restore/after_restore.sh
+++ deployment/shared/restore/after_restore.sh
@@ -23,6 +23,10 @@ EOF
 )
 
 echo "Executing clearing sensitive data!!!!"
+echo "Executing clearing sensitive data!!!!"
+echo "Executing clearing sensitive data!!!!"
+echo "Executing clearing sensitive data!!!!"
+echo $sql
 
 PGPASSWORD="${POSTGRES_PASSWORD}" psql ${URL} -U ${POSTGRES_USER} -c "$sql"
 

b5715f40fb540e328f16868119c9471848823150
Mateusz Kwiatkowski <mkwiatkowski@soldevelo.com>
2019-03-26 19:02:47 +0100

    AO-96: added clearing images

--- deployment/shared/restart_or_restore.sh
+++ deployment/shared/restart_or_restore.sh
@@ -4,6 +4,12 @@ if [ "$KEEP_OR_WIPE" == "wipe" ]; then
     echo "Restoring database from the latest snapshot"
     cp ../../credentials/${CREDENTIALS_SUB_DIRECTORY}/.env-restore ../shared/restore/.env-restore
 
+    docker images
+
+    docker stop $(docker ps -a -q)
+    docker rm $(docker ps -a -q)
+    docker rmi $(docker images -q)
+
     /usr/local/bin/docker-compose down -v
     /usr/local/bin/docker-compose -f ../shared/restore/docker-compose.yml run rds-restore
     /usr/local/bin/docker-compose up --build --force-recreate -d

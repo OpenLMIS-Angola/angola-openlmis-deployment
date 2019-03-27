03a166fe91ea5fe27a6f47f8d2dac731ee6bf7f3
Mateusz Kwiatkowski <mkwiatkowski@soldevelo.com>
2019-03-27 11:57:37 +0100

    AO-96: added building docker compose images with no cache

--- deployment/shared/restart_or_restore.sh
+++ deployment/shared/restart_or_restore.sh
@@ -11,6 +11,7 @@ if [ "$KEEP_OR_WIPE" == "wipe" ]; then
     docker rmi $(docker images -q)
 
     /usr/local/bin/docker-compose down -v
+    /usr/local/bin/docker-compose -f ../shared/restore/docker-compose.yml build --no-cache
     /usr/local/bin/docker-compose -f ../shared/restore/docker-compose.yml run rds-restore
     /usr/local/bin/docker-compose up --build --force-recreate -d
 

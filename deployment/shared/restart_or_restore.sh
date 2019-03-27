21d10bdae5f2a7a15ff2cf815726679315da3ceb
Mateusz Kwiatkowski <mkwiatkowski@soldevelo.com>
2019-03-27 16:39:22 +0100

    AO-96: removed clearing docker images

--- deployment/shared/restart_or_restore.sh
+++ deployment/shared/restart_or_restore.sh
@@ -4,14 +4,6 @@ if [ "$KEEP_OR_WIPE" == "wipe" ]; then
     echo "Restoring database from the latest snapshot"
     cp ../../credentials/${CREDENTIALS_SUB_DIRECTORY}/.env-restore ../shared/restore/.env-restore
 
-    docker images
-
-    docker stop $(docker ps -a -q)
-    docker rm $(docker ps -a -q)
-    docker rmi $(docker images -q)
-
-    docker system prune -a --volumes -f
-
     /usr/local/bin/docker-compose down -v
     /usr/local/bin/docker-compose -f ../shared/restore/docker-compose.yml build --no-cache
     /usr/local/bin/docker-compose -f ../shared/restore/docker-compose.yml run rds-restore

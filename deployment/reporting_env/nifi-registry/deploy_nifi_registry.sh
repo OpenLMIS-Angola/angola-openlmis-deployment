46e480a1b1d2416db9fb0f1d5292df78baf9b77e
Daniel Serkowski <dserkowski@soldevelo.com>
2019-04-08 12:16:13 +0200

    AO-156: Adjusted configuration for nifi-registry to development server

--- deployment/reporting_env/nifi-registry/deploy_nifi_registry.sh
+++ deployment/reporting_env/nifi-registry/deploy_nifi_registry.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 export DOCKER_TLS_VERIFY="1"
-export DOCKER_HOST="tcp://nifi-registry.openlmis.org:2376"
-export DOCKER_CERT_PATH="${PWD}/credentials"
+export DOCKER_HOST="development.ao.openlmis.org:2376"
+export DOCKER_CERT_PATH="${PWD}/../../../credentials/dev_env"
 
 cp -r $credentials ./credentials
 /usr/local/bin/docker-compose kill
 /usr/local/bin/docker-compose down -v
-/usr/local/bin/docker-compose up --build --force-recreate -d
+/usr/local/bin/docker-compose up --build --force-recreate -d --scale scalyr=0

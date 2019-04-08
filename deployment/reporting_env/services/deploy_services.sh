4afd3bcbb55a4afbac476f6c0bdcd24b25f129e1
Daniel Serkowski <dserkowski@soldevelo.com>
2019-04-08 09:41:48 +0200

    AO-156: Adjusted configuration of deploying nifi and superset to development server

--- deployment/reporting_env/services/deploy_services.sh
+++ deployment/reporting_env/services/deploy_services.sh
@@ -1,27 +1,28 @@
 #!/usr/bin/env bash
 
 export DOCKER_TLS_VERIFY="1"
-export DOCKER_HOST="tcp://nifi-registry.openlmis.org:2376"
-export DOCKER_CERT_PATH="${PWD}/credentials"
-export DOCKER_COMPOSE_BIN=/usr/local/bin/docker-compose
+export DOCKER_HOST="development.ao.openlmis.org:2376"
+export DOCKER_CERT_PATH="${PWD}/../../../credentials/dev_env"
+
 export REPORTING_DIR_NAME=reporting
 
-export SUPERSET_ENABLE_SSL=true
-export SUPERSET_SSL_CERT_CHAIN=reporting-uat/ssl-fullchain-reporting.uat.openlmis.org.pem
-export SUPERSET_SSL_KEY=reporting-uat/ssl-privkey-reporting.uat.openlmis.org.pem
-export SUPERSET_SSL_CERT=reporting-uat/ssl-fullchain-reporting.uat.openlmis.org.pem
-export SUPERSET_DOMAIN_NAME=superset.uat.openlmis.org
-export NIFI_ENABLE_SSL=true
-export NIFI_SSL_CERT_CHAIN=reporting-uat/ssl-fullchain-reporting.uat.openlmis.org.pem
-export NIFI_SSL_KEY=reporting-uat/ssl-privkey-reporting.uat.openlmis.org.pem
-export NIFI_SSL_CERT=reporting-uat/ssl-fullchain-reporting.uat.openlmis.org.pem
-export NIFI_DOMAIN_NAME=nifi.uat.openlmis.org
+export SUPERSET_ENABLE_SSL=false
+export SUPERSET_SSL_CERT_CHAIN=
+export SUPERSET_SSL_KEY=
+export SUPERSET_SSL_CERT=
+export SUPERSET_DOMAIN_NAME=development.ao.openlmis.org
+
+export NIFI_ENABLE_SSL=false
+export NIFI_SSL_CERT_CHAIN=
+export NIFI_SSL_KEY=
+export NIFI_SSL_CERT=
+export NIFI_DOMAIN_NAME=development.ao.openlmis.org
 
 reportingRepo=$1
 
-cp -r "$credentials" "$DOCKER_CERT_PATH"
-cp -r "$reporting_ssl" "$reportingRepo/$REPORTING_DIR_NAME/config/services/nginx/tls/reporting-uat"
 cd "$reportingRepo/$REPORTING_DIR_NAME"
+
+export DOCKER_COMPOSE_BIN=/usr/local/bin/docker-compose
 $DOCKER_COMPOSE_BIN kill
 $DOCKER_COMPOSE_BIN down -v
-$DOCKER_COMPOSE_BIN up --build --force-recreate -d
+$DOCKER_COMPOSE_BIN up --build --force-recreate -d --scale scalyr=0

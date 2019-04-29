56e11258deb763b53706e3e4bd863a9071ccb78d
Mateusz Kwiatkowski <mkwiatkowski@soldevelo.com>
2019-04-30 01:48:53 +0200

    AO-242: changed prod reporting stack URLs to domain

--- deployment/reporting_producton_env/nifi-registry/deploy_nifi_registry.sh
+++ deployment/reporting_producton_env/nifi-registry/deploy_nifi_registry.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 export DOCKER_TLS_VERIFY="1"
-export DOCKER_HOST="108.128.10.51:2376"
+export DOCKER_HOST="report.production.ao.openlmis.org:2376"
 export DOCKER_CERT_PATH="${PWD}/../../../credentials/production_reporting_env"
 
 /usr/local/bin/docker-compose kill
--- deployment/reporting_producton_env/services/deploy_services.sh
+++ deployment/reporting_producton_env/services/deploy_services.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 export DOCKER_TLS_VERIFY="1"
-export DOCKER_HOST="108.128.10.51:2376"
+export DOCKER_HOST="report.production.ao.openlmis.org:2376"
 export DOCKER_CERT_PATH="${PWD}/../../../credentials/production_reporting_env"
 
 export REPORTING_DIR_NAME=reporting
@@ -10,13 +10,13 @@ export SUPERSET_ENABLE_SSL=false
 export SUPERSET_SSL_CERT_CHAIN=
 export SUPERSET_SSL_KEY=
 export SUPERSET_SSL_CERT=
-export SUPERSET_DOMAIN_NAME=108.128.10.51
+export SUPERSET_DOMAIN_NAME=report.production.ao.openlmis.org
 
 export NIFI_ENABLE_SSL=false
 export NIFI_SSL_CERT_CHAIN=
 export NIFI_SSL_KEY=
 export NIFI_SSL_CERT=
-export NIFI_DOMAIN_NAME=108.128.10.51
+export NIFI_DOMAIN_NAME=report.production.ao.openlmis.org
 export NIFI_JVM_HEAP_MAX=2g
 
 reportingRepo=$1

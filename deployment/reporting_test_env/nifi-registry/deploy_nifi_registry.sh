4a9b136163765301effc0d21e5cc7f46d8b928fe
Daniel Serkowski <serkowskidaniel@gmail.com>
2019-05-06 15:53:54 +0200

    AO-156: Refactor -> renamed reporting test env directory

--- deployment/reporting_test_env/nifi-registry/deploy_nifi_registry.sh
+++ deployment/reporting_test_env/nifi-registry/deploy_nifi_registry.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 export DOCKER_TLS_VERIFY="1"
+export COMPOSE_TLS_VERSION=TLSv1_2
 export DOCKER_HOST="report.test.ao.openlmis.org:2376"
 export DOCKER_CERT_PATH="${PWD}/../../../credentials/test_reporting_env"
 

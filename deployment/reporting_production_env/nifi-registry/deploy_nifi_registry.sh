dc8631b80a0278dbe73dcaf56d25f0ac91cce45b
Daniel Serkowski <serkowskidaniel@gmail.com>
2019-05-27 17:06:01 +0200

    AO-279: Synchronized with core changes of ReportingStack

--- deployment/reporting_production_env/nifi-registry/deploy_nifi_registry.sh
+++ deployment/reporting_production_env/nifi-registry/deploy_nifi_registry.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
+set -e
 export DOCKER_TLS_VERIFY="1"
+export COMPOSE_TLS_VERSION=TLSv1_2
 export DOCKER_HOST="report.production.ao.openlmis.org:2376"
 export DOCKER_CERT_PATH="${PWD}/../../../credentials/production_reporting_env"
 
--- deployment/reporting_production_env/services/deploy_services.sh
+++ deployment/reporting_production_env/services/deploy_services.sh
@@ -4,27 +4,24 @@ export DOCKER_TLS_VERIFY="1"
 export COMPOSE_TLS_VERSION=TLSv1_2
 export DOCKER_HOST="report.production.ao.openlmis.org:2376"
 export DOCKER_CERT_PATH="${PWD}/../../../credentials/production_reporting_env"
+export DOCKER_COMPOSE_BIN=/usr/local/bin/docker-compose
 
 export REPORTING_DIR_NAME=reporting
 
 export SUPERSET_ENABLE_SSL=false
-export SUPERSET_SSL_CERT_CHAIN=
-export SUPERSET_SSL_KEY=
-export SUPERSET_SSL_CERT=
+export SUPERSET_BEHIND_LOAD_BALANCER=false
+export SUPERSET_LOAD_BALANCER_REDIRECT_HTTP=false
 export SUPERSET_DOMAIN_NAME=report.production.ao.openlmis.org
 
 export NIFI_ENABLE_SSL=false
-export NIFI_SSL_CERT_CHAIN=
-export NIFI_SSL_KEY=
-export NIFI_SSL_CERT=
+export NIFI_BEHIND_LOAD_BALANCER=false
+export NIFI_LOAD_BALANCER_REDIRECT_HTTP=false
 export NIFI_DOMAIN_NAME=report.production.ao.openlmis.org
-export NIFI_JVM_HEAP_MAX=2g
 
 reportingRepo=$1
 
 cd "$reportingRepo/$REPORTING_DIR_NAME"
 
-export DOCKER_COMPOSE_BIN=/usr/local/bin/docker-compose
 $DOCKER_COMPOSE_BIN kill
 $DOCKER_COMPOSE_BIN down -v
 $DOCKER_COMPOSE_BIN up --build --force-recreate -d --scale scalyr=0 --scale kafka=0 --scale zookeeper=0

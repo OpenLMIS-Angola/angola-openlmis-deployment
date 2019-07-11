88a7f9b296772c78d3edf49f6b46626ee279e8f7
Mateusz Kwiatkowski <mkwiatkowski@soldevelo.com>
2019-07-11 21:57:35 +0200

    AO-331: removed oinserting superset rights

--- deployment/dev_env/deploy_to_dev_env.sh
+++ deployment/dev_env/deploy_to_dev_env.sh
@@ -8,5 +8,3 @@ export DOCKER_CERT_PATH="${PWD}/../../credentials/${CREDENTIALS_SUB_DIRECTORY}"
 /usr/local/bin/docker-compose pull
 
 ../shared/tmp_run.sh $1
-
-../shared/superset_rights/insert_superset_rights.sh

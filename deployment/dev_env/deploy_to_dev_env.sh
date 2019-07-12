70a160903db9da40fb67a7cf7a5cf17277115bf6
Mateusz Wedeł <mwedel@soldevelo.com>
2019-07-12 09:59:51 +0200

    AO-331 Remove loading env file.

--- deployment/dev_env/deploy_to_dev_env.sh
+++ deployment/dev_env/deploy_to_dev_env.sh
@@ -8,9 +8,3 @@ export DOCKER_CERT_PATH="${PWD}/../../credentials/${CREDENTIALS_SUB_DIRECTORY}"
 /usr/local/bin/docker-compose pull
 
 ../shared/tmp_run.sh $1
-
-set -a
-. ./settings.env
-set +a
-
-../shared/superset_rights/insert_superset_rights.sh

bbb61d902c4b11c18741f53a38d05522f3f0b246
Mateusz Kwiatkowski <mkwiatkowski@soldevelo.com>
2019-08-16 12:40:10 +0200

    AO-384: updated domain name for development server

--- deployment/dev_env/deploy_to_dev_env.sh
+++ deployment/dev_env/deploy_to_dev_env.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 export DOCKER_TLS_VERIFY="1"
-export DOCKER_HOST="development.ao.openlmis.org:2376"
+export DOCKER_HOST="dev.siglofa.sisangola.org:2376"
 export CREDENTIALS_SUB_DIRECTORY="dev_env"
 export DOCKER_CERT_PATH="${PWD}/../../credentials/${CREDENTIALS_SUB_DIRECTORY}"
 

2fd46c10dd1f735e4105453cd1469f31fa62ae90
Mateusz Kwiatkowski <mkwiatkowski@soldevelo.com>
2019-08-16 13:49:12 +0200

    AO-203: updated domain name for test server

--- deployment/test_env/deploy_to_test_env.sh
+++ deployment/test_env/deploy_to_test_env.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 export DOCKER_TLS_VERIFY="1"
-export DOCKER_HOST="test.ao.openlmis.org:2376"
+export DOCKER_HOST="test.siglofa.sisangola.org:2376"
 export CREDENTIALS_SUB_DIRECTORY="test_env"
 export DOCKER_CERT_PATH="${PWD}/../../credentials/${CREDENTIALS_SUB_DIRECTORY}"
 

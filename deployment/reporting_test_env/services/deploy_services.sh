b5743005c7fd13689df121ed8947b03369b6e7d0
Daniel Serkowski <dserkowski@soldevelo.com>
2019-08-16 16:09:15 +0200

    AO-203: Updated test ReportingStack server domain name

--- deployment/reporting_test_env/services/deploy_services.sh
+++ deployment/reporting_test_env/services/deploy_services.sh
@@ -2,7 +2,7 @@
 
 export DOCKER_TLS_VERIFY="1"
 export COMPOSE_TLS_VERSION=TLSv1_2
-export DOCKER_HOST="report.test.ao.openlmis.org:2376"
+export DOCKER_HOST="report.test.siglofa.sisangola.org:2376"
 export DOCKER_CERT_PATH="${PWD}/../../../credentials/test_reporting_env"
 export DOCKER_COMPOSE_BIN=/usr/local/bin/docker-compose
 

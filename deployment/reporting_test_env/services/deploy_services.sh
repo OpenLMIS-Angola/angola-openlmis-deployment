a51d6eb8782aeb3da1313f785e844be3a594f6a5
Mateusz Wedeł <mwedel@soldevelo.com>
2019-08-23 10:41:20 +0200

    AO-425 Remove lets encrypt settings from test instance

--- deployment/reporting_test_env/services/deploy_services.sh
+++ deployment/reporting_test_env/services/deploy_services.sh
@@ -8,14 +8,25 @@ export DOCKER_COMPOSE_BIN=/usr/local/bin/docker-compose
 
 export REPORTING_DIR_NAME=reporting
 
+export SUPERSET_ENABLE_SSL=false
+export SUPERSET_BEHIND_LOAD_BALANCER=false
+export SUPERSET_LOAD_BALANCER_REDIRECT_HTTP=false
+#export SUPERSET_DOMAIN_NAME=report.test.ao.openlmis.org
+
+export NIFI_ENABLE_SSL=false
+export NIFI_BEHIND_LOAD_BALANCER=false
+export NIFI_LOAD_BALANCER_REDIRECT_HTTP=false
+#export NIFI_DOMAIN_NAME=report.test.ao.openlmis.org
+
+
 distro_repo=$1
-init_with_lets_encrypt_sh_path="../../deployment/shared/init_with_lets_encrypt.sh"
+#init_with_lets_encrypt_sh_path="../../deployment/shared/init_with_lets_encrypt.sh"
 
 # In order to avoid generated new certificates between next deploys of ReportingStack
 # we need to move them to seperate volume marked as external.
 # External volumes are not removed even we use docker-compose down with -v option.
 # The external volume need to be created before the docker start
-docker volume create letsencrypt-config
+#docker volume create letsencrypt-config
 # The same is with data stored by database. To avoid running the whole ETL process,
 # we need to create a volume for Postgres data and mark it as external,
 # so that Nifi can update already persisted data.
@@ -26,6 +37,5 @@ $DOCKER_COMPOSE_BIN kill &&
 $DOCKER_COMPOSE_BIN down -v --remove-orphans &&
 
 /usr/local/bin/docker-compose build &&
-. $init_with_lets_encrypt_sh_path &&
 
 $DOCKER_COMPOSE_BIN up -d --scale scalyr=0

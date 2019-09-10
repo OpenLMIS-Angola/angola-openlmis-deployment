df6da7a24b5afccbe5d28c4d4a799223e4200380
Daniel Serkowski <dserkowski@soldevelo.com>
2019-09-10 13:55:19 +0200

    AO-498:  Added deployment scripts for localInstallationTest

---
+++ deployment/local_installation_test_env/deploy_to_local_installation_test_env.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+export DOCKER_TLS_VERIFY="1"
+export DOCKER_HOST="local.ao.openlmis.org:2376"
+export CREDENTIALS_SUB_DIRECTORY="local_installation_test_env"
+export DOCKER_CERT_PATH="${PWD}/../../credentials/${CREDENTIALS_SUB_DIRECTORY}"
+
+/usr/local/bin/docker-compose pull
+
+../shared/restart.sh $1

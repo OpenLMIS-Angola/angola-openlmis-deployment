6551146f6afc60b59f953d00203ef3ce33d83128
Daniel Serkowski <dserkowski@soldevelo.com>
2019-09-11 10:29:26 +0200

    AO-498: Exporting of required fields from settings.env

--- deployment/local_installation_test_env/after_start.sh
+++ deployment/local_installation_test_env/after_start.sh
@@ -4,7 +4,7 @@
 : ${POSTGRES_USER:?"Need to set POSTGRES_USER"}
 : ${POSTGRES_PASSWORD:?"Need to set POSTGRES_PASSWORD"}
 
-: ${auth.server.clientSecret:?"Need to set auth.server.clientSecret"}
+: ${UI_CLIENT_SECRET:?"Need to set UI_CLIENT_SECRET"}
 : ${AUTH_SERVER_CLIENT_SECRET:?"Need to set AUTH_SERVER_CLIENT_SECRET"}
 : ${OL_SUPERSET_PASSWORD:?"Need to set OL_SUPERSET_PASSWORD"}
 
@@ -18,7 +18,7 @@ DELETE FROM auth.oauth_client_details WHERE clientid = 'angola-client';
 DELETE FROM auth.oauth_client_details WHERE clientid = 'angola-ui-client';
 DELETE FROM auth.oauth_client_details WHERE clientid = 'superset';
 
-INSERT INTO auth.oauth_client_details (clientId,authorities,authorizedGrantTypes,clientSecret,"scope","resourceIds") VALUES ('angola-client','TRUSTED_CLIENT','client_credentials','${auth.server.clientSecret}','read,write','hapifhir,notification,reports,diagnostics,auth,requisition,referencedata,stockmanagement,angola-reference-ui,fulfillment');
+INSERT INTO auth.oauth_client_details (clientId,authorities,authorizedGrantTypes,clientSecret,"scope","resourceIds") VALUES ('angola-client','TRUSTED_CLIENT','client_credentials','${UI_CLIENT_SECRET}','read,write','hapifhir,notification,reports,diagnostics,auth,requisition,referencedata,stockmanagement,angola-reference-ui,fulfillment');
 INSERT INTO auth.oauth_client_details (clientId,authorities,authorizedGrantTypes,clientSecret,"scope","resourceIds") VALUES ('angola-ui-client','TRUSTED_CLIENT','password','${AUTH_SERVER_CLIENT_SECRET}','read,write','hapifhir,notification,reports,diagnostics,auth,requisition,referencedata,stockmanagement,angola-reference-ui,fulfillment');
 INSERT INTO auth.oauth_client_details (clientId,authorities,authorizedGrantTypes,clientSecret,"scope","resourceIds") VALUES ('superset','TRUSTED_CLIENT','authorization_code','${OL_SUPERSET_PASSWORD}','read,write','hapifhir,notification,reports,diagnostics,auth,requisition,referencedata,stockmanagement,angola-reference-ui,fulfillment');
 EOF
--- deployment/local_installation_test_env/deploy_to_local_installation_test_env.sh
+++ deployment/local_installation_test_env/deploy_to_local_installation_test_env.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+read_var_from_settings_env() {
+    echo $(cat settings.env | grep "$1" | cut -d '=' -f2)
+}
+
 export DOCKER_TLS_VERIFY="1"
 export DOCKER_HOST="local.ao.openlmis.org:2376"
 export CREDENTIALS_SUB_DIRECTORY="local_installation_test_env"
@@ -7,5 +11,12 @@ export DOCKER_CERT_PATH="${PWD}/../../credentials/${CREDENTIALS_SUB_DIRECTORY}"
 
 /usr/local/bin/docker-compose pull
 
+export DATABASE_URL=$(read_var_from_settings_env DATABASE_URL)
+export POSTGRES_USER=$(read_var_from_settings_env POSTGRES_USER)
+export POSTGRES_PASSWORD=$(read_var_from_settings_env POSTGRES_PASSWORD)
+export OL_SUPERSET_PASSWORD=$(read_var_from_settings_env OL_SUPERSET_PASSWORD)
+export AUTH_SERVER_CLIENT_SECRET=$(read_var_from_settings_env AUTH_SERVER_CLIENT_SECRET)
+export UI_CLIENT_SECRET=$(read_var_from_settings_env auth.server.clientSecret)
 . after_start.sh &&
+
 ../shared/restart.sh $1

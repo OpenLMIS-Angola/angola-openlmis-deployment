e3ee2840b5d07622f05b335fb15dcaf71820d5e3
Mateusz Wedeł <mwedel@soldevelo.com>
2019-10-03 16:11:57 +0200

    AO-534: Add waiting for database before clearing sensitive data.

--- deployment/shared/restore/after_restore.sh
+++ deployment/shared/restore/after_restore.sh
@@ -21,6 +21,11 @@ UPDATE auth.oauth_client_details SET clientsecret = '${CLIENT_SECRET}' WHERE cli
 EOF
 )
 
+until PGPASSWORD="${POSTGRES_PASSWORD}" psql ${URL} -U ${POSTGRES_USER} -c '\q'; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 5
+done
+
 echo "Executing clearing sensitive data!!!!"
 
 PGPASSWORD="${POSTGRES_PASSWORD}" psql ${URL} -U ${POSTGRES_USER} -c "$sql"

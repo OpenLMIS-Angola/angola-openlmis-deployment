14c0517b44ae432b61cbc6b56c646af14a986f33
Daniel Serkowski <dserkowski@soldevelo.com>
2019-10-31 15:51:37 +0100

    Improved restart script to be sure that internal volumes are removed during restart

--- deployment/shared/restart.sh
+++ deployment/shared/restart.sh
@@ -3,7 +3,8 @@
 WIPE_MSG="Will WIPE data!"
 KEEP_MSG="Will keep data."
 
-/usr/local/bin/docker-compose down
+/usr/local/bin/docker-compose kill
+/usr/local/bin/docker-compose down -v
 
 if [ "$KEEP_OR_WIPE" == "wipe" ]; then
   echo "$WIPE_MSG"

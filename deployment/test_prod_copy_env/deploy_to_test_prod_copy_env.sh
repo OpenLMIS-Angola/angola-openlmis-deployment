ac578bb84275061f38af4998e23ec8cdb725ee35
Paweł Gesek <pgesek@soldevelo.com>
2022-08-25 18:31:37 +0200

    Update IP for test prod copy instance

--- deployment/test_prod_copy_env/deploy_to_test_prod_copy_env.sh
+++ deployment/test_prod_copy_env/deploy_to_test_prod_copy_env.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 export DOCKER_TLS_VERIFY="1"
-export DOCKER_HOST="3.249.240.144:2376"
+export DOCKER_HOST="54.246.37.155:2376"
 export DOCKER_CERT_PATH="${PWD}/../../credentials/test_prod_copy_env"
 
 /usr/local/bin/docker-compose pull

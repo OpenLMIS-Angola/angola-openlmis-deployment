6cc3ade4dac9cbd21bccf2b9929ead7a971bfa85
Paweł Gesek <pgesek@soldevelo.com>
2022-08-26 11:53:33 +0200

    Fix Docker cert path for reporting prod copy

--- deployment/test_prod_copy_reporting_env/services/deploy_services.sh
+++ deployment/test_prod_copy_reporting_env/services/deploy_services.sh
@@ -3,7 +3,7 @@
 export DOCKER_TLS_VERIFY="1"
 export COMPOSE_TLS_VERSION=TLSv1_2
 export DOCKER_HOST="52.212.136.33:2376"
-export DOCKER_CERT_PATH="${PWD}/../../../credentials/production_reporting_env"
+export DOCKER_CERT_PATH="${PWD}/../../../credentials/test_prod_copy_reporting_env"
 export DOCKER_COMPOSE_BIN=/usr/local/bin/docker-compose
 
 export REPORTING_DIR_NAME=reporting

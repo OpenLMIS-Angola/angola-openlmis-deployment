4ccb84fabee5716a95d72885888e8dc6815bcf71
Mateusz Sobolak <msobolak@soldevelo.com>
2023-10-17 10:55:11 +0200

    Add files via upload

--- deployment/test_reporting_on_prem_env/deploy_to_test_env.sh
+++ deployment/test_reporting_on_prem_env/deploy_to_test_env.sh
@@ -1,1 +1,10 @@
+#!/usr/bin/env bash
 
+export DOCKER_TLS_VERIFY="1"
+export DOCKER_HOST="172.18.130.75:2376"
+export CREDENTIALS_SUB_DIRECTORY="test_reporting_on_prem_env"
+export DOCKER_CERT_PATH="${PWD}/../../credentials/${CREDENTIALS_SUB_DIRECTORY}"
+
+/usr/local/bin/docker-compose pull
+
+../shared/restart_or_restore.sh $1
